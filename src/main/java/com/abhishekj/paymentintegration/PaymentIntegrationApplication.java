bc20ef16aa1490de36ed6812117edb833390cfbb
Akash Jaiswal <57207483+akashj009@users.noreply.github.com>
2021-07-22 00:09:29 +0530

    new project added

--- src/main/java/com/abhishekj/paymentintegration/PaymentIntegrationApplication.java
+++ src/main/java/com/abhishekj/paymentintegration/PaymentIntegrationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentIntegrationApplication {
 
 	public static void main(String[] args) {
+		System.out.println("heloo");
 		SpringApplication.run(PaymentIntegrationApplication.class, args);
 	}
 
