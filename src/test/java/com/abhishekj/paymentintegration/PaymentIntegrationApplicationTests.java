a071febe9fb15d0807d28dcc5d0483c00f7c00cd
Akash Jaiswal <57207483+akashj009@users.noreply.github.com>
2021-07-21 23:39:52 +0530

    new project added

---
+++ src/test/java/com/abhishekj/paymentintegration/PaymentIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishekj.paymentintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
